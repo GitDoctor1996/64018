b139c70b54bd30642a5a7e7124839d6e0be7a40a
GitDoctor1996 <40804745+GitDoctor1996@users.noreply.github.com>
2019-09-08 18:08:11 -0400

    Add files via upload

---
+++ Assignments/HW1/810895300_1.R
@@ -0,0 +1,15 @@
+#Reading the data
+
+data_GDP <- read.csv("C:/Users/nicho/Google Drive/School/Graduate School
+                 /Kent State/Fall 2019/Quantitative Management Modeling
+                 /HW/HW1 Data.csv") 
+
+View(data_GDP)
+
+#Creating the Summary Statistics
+
+summary(data_GDP)
+
+#Creating the time series graph
+
+plot(x=data_GDP$DATE, y=data_GDP$GDP_Growth, type='b', col='#ff0000', xlab='Date', ylab='GDP Growth', main='US GDP Growth Trends')
