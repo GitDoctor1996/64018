c499c37ed846673c6bc0288990c15bb69eb563f3
GitDoctor1996 <40804745+GitDoctor1996@users.noreply.github.com>
2019-09-08 22:02:11 -0400

    Add files via upload

--- Assignments/HW1/810895300_1.R
+++ Assignments/HW1/810895300_1.R
@@ -12,4 +12,6 @@ summary(data_GDP)
 
 #Creating the time series graph
 
+data_GDP$DATE <- as.Date(data_GDP$DATE,'%m/%d/%Y') 
+
 plot(x=data_GDP$DATE, y=data_GDP$GDP_Growth, type='b', col='#ff0000', xlab='Date', ylab='GDP Growth', main='US GDP Growth Trends')
